8866464d0c8935c9c935a6240330e1ce824afa49
Saneec <crisman2005@gmail.com>
2025-11-06 17:05:16 -0500

    Created Mobile Navbar component

---
+++ src/MobileNavBar.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+export default function MobileNavBar() {
+    const [nightModeToggle, setNightModeToggle] = useState(true);
+    return (
+        <div>
+            <a><img /></a>
+            <a><img /></a>
+            <a><img /></a>
+            <a><img /></a>
+            <a><img /></a>
+
+        </div>
+    )
+}
