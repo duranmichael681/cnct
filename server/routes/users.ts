be3a8f76302e03ef4958fa7b63c6543de5efbed3
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2025-11-05 20:43:06 -0500

    Created dummy middleware for route 'api/users/:id' to experiment with Express.JS and TypeScript

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,10 +1,16 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 //import other necessary middlewares and controllrs here
 
 const userRouter = Router();
 
 //TODO: add middlewares and controllers later
-userRouter.get('/users/:id', /*middleware, controller*/)
+userRouter.get('/users/:id', function(request: Request, response: Response) {
+    try {
+        Response.send(request.params.id);
+    } catch (error: any) {
+        Response.send("User not found");
+    }
+})
 userRouter.put('/users/:id', /*middleware, controller*/)
 userRouter.get('/users/:id/events', /*middleware, controller*/)
 userRouter.get('/users/:id/friends', /*middleware, controller*/)
