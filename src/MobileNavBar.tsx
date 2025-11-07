cc108fb973ffbafc82c2f4feee785b62156ccef1
Saneec <crisman2005@gmail.com>
2025-11-07 10:27:14 -0500

    Worked on MobileNavBar

--- src/MobileNavBar.tsx
+++ src/MobileNavBar.tsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
+import HomeIcon from './components/icons/HomeIcon.tsx'
+import ExploreIcon from './components/icons/ExploreIcon.tsx'
+import ProfileIcon from './components/icons/ProfileIcon.tsx'
+import CreateIcon from './components/icons/CreateIcon.tsx'
 
 export default function MobileNavBar() {
     const [nightModeToggle, setNightModeToggle] = useState(true);
     return (
-        <div>
-            <a><img /></a>
-            <a><img /></a>
-            <a><img /></a>
-            <a><img /></a>
-            <a><img /></a>
-
+        <div className="h-dvh">
+        <div className="gap-56/360flex flex-row bottom-0 left-0 w-full bg-[#081E3F] h-3/32">
+            <a><HomeIcon /></a>
+            <a><ExploreIcon /></a>
+            <a><ProfileIcon /></a>
+            <a><CreateIcon /></a>
+            <a></a>
+        </div>
         </div>
     )
 }
