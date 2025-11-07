5e68e13cda9c90ca4cdbed7de55e6fc32ee371d9
Michael Duran <duranmichael681@gmail.com>
2025-11-07 14:27:16 -0500

    Created a collage on the right side of the screen instead of just static photos. Also cleaned some of the styling issues with use of Gemini. Some of it may not be right but we can always go back to it just wanted to add this nice to have

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home/home'
 import SignUp from './auth/Signup'
+import SignIn from './auth/Signin'
 
 export default function App() {
   return (
     <Routes>
       <Route path='/' element={<SignUp />} />
+      <Route path='/Signin' element={<SignIn />} />
       <Route path='/Home' element={<Home />} />
     </Routes>
   )
