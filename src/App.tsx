3b6a83daa9efe6c5031ed845814d5986e15b843c
Saneec <crisman2005@gmail.com>
2025-11-07 13:41:21 -0500

    Finished and refactored MobileNavbar to Router
    The mobile navbar is finished. You can view it at "/MobileNav"  when hosting the server.
    To view on your mobile device, go to vite.config.ts and after the plugins key in the define function, put
    
    server: {
        host: true,
        port: 3000,
      }
    
    and then visit the page on your mobile device using the ip shown in the console.
    
    To integrate the navbar onto other pages, make the route element of the page a child of the navbar's route element (In between the tags).
    
    The only thing left to do is to Link the buttons to the correct pages once they're created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home/home'
 import SignUp from './auth/Signup'
+import MobileNavBar from './pages/Home/Navbars/MobileNavBar'
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/MobileNav" element={<MobileNavBar />}>
+
+      </Route>
       <Route path='/' element={<SignUp />} />
       <Route path='/Home' element={<Home />} />
     </Routes>
