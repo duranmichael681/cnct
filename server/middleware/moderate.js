c2603e07e8a3fb9bd4bab88b771550606e26f5b5
Matthew Fortes <77417435+Mattjava@users.noreply.github.com>
2025-12-04 22:27:06 -0500

    Updated image moderation to better detect NSFW content and self-harm

--- server/middleware/moderate.js
+++ server/middleware/moderate.js
@@ -13,10 +13,12 @@ dotenv.config();
 
 const api_endpoint = "https://api.sightengine.com/1.0/check.json";
 
+const threshold = 0.5;
+
 export async function moderateImage(url) {
     const params = {
         'url': url,
-        'models': 'nudity-2.1,recreational_drug,gore-2.0,violence',
+        'models': 'nudity-2.1,recreational_drug,gore-2.0,violence,self-harm',
         'api_user': process.env.SIGHTENGINE_USER,
         'api_secret': process.env.SIGHTENGINE_SECRET,
     }
@@ -28,19 +30,41 @@ export async function moderateImage(url) {
 
         const nudity_content = content_data['nudity'];
 
-        Object.keys(nudity_content).forEach(key => {
-            if(!key.endsWith("none") && nudity_content[key] > 0.8) {
-                return false;
-            }
-        })
+        const nudity_flag = search(nudity_content);
 
-        if(content_data['gore']['prob'] > 0.8) {
+        if(nudity_flag)
+            return false;
+
+        if(content_data['gore']['prob'] > threshold) {
             return false;
         }
+
+        if(content_data['self-harm']['prob'] > threshold)
+            return false;
 
         return true;
     } catch (error) {
         console.log(error);
         return false;
     }
+}
+
+async function search(map) {
+    var flag = false;
+
+    Object.keys(map).forEach(key => {
+        if(key.endsWith('none') || key.endsWith('context'))
+            return;
+
+        var value = map[key];
+
+
+        if(typeof value != 'number' && !search(value))
+            flag = true;
+
+        else if(value > threshold)
+            flag = true;
+    })
+
+    return flag;
 }
